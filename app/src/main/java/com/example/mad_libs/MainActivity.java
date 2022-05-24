18e351c242ebd4e0597236eb9e733128aced27ff
gilbertKorir <jerikorir@gmail.com>
2022-05-24 22:34:43 +0300

    added bindviews

--- app/src/main/java/com/example/mad_libs/MainActivity.java
+++ app/src/main/java/com/example/mad_libs/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.mad_libs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.personEditText) EditText mPerson;
+    @BindView(R.id.colorEditText) EditText mColor;
+    @BindView(R.id.foodEditText) EditText mFood;
+    @BindView(R.id.adjective) EditText mAdjective;
+    @BindView(R.id.thingEditText) EditText mThing;
+    @BindView(R.id.button) EditText mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
